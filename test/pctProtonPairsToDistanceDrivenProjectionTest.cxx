2679dbbfe404db10f5618c83dec3ecafb9604600
Lucas Gandel <lucas.gandel@kitware.com>
2025-02-20 07:55:37 +0100

    STYLE: Update Copyright

--- test/pctProtonPairsToDistanceDrivenProjectionTest.cxx
+++ test/pctProtonPairsToDistanceDrivenProjectionTest.cxx
@@ -1,6 +1,6 @@
 /*=========================================================================
  *
- *  Copyright RTK Consortium
+ *  Copyright Centre National de la Recherche Scientifique
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
